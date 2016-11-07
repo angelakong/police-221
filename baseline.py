ae7617fff8cde826802c142e30f5099f6a147d02
Michelle Lam <michelle123lam@gmail.com>
2016-11-06 22:56:47 -0800

    Adds CSV data files

--- baseline.py
+++ baseline.py
@@ -18,14 +18,16 @@ def importData():
     officer = dict()
     complainants = dict()
     pt_ct = 0
-    # {'1': ['officer_M', 'officer_White', 'PO'], '2': ['officer_F', 'officer_Hispanic', 'PO']}
+    # {'1': ['officer_M', 'officer_White', 'PO'],
+    #  '2': ['officer_F', 'officer_Hispanic', 'PO']}
     for row in officers_sheet.iter_rows(row_offset=1):
         officer[str(row[0].value)] = ["officer_" + str(row[1].value), "officer_" + str(row[2].value), str(row[3].value)]
-    # {'1043921': ['complain_F', 'complain_White/Hispanic'], '1043909': ['complain_F', 'complain_Black']}
+    # {'1043921': ['complain_F', 'complain_White/Hispanic'],
+    #  '1043909': ['complain_F', 'complain_Black']}
     for row in complainants_sheet.iter_rows(row_offset=1):
         complainants[str(row[1].value)] = ["complain_" + str(row[2].value), "complain_" + str(row[5].value)]
     for row in allegations_sheet.iter_rows(row_offset=1):
-        if row[1].value != None and row[2].value != None and row[3].value != None and row[6].value != None: 
+        if row[1].value != None and row[2].value != None and row[3].value != None and row[6].value != None:
             all_info = list()
             if str(row[1].value) in complainants and str(row[2].value) in officer:
                 complainant_info = complainants[str(row[1].value)]
@@ -54,7 +56,7 @@ def learnPredictor(trainExamples, testExamples, featureExtractor, numIters, eta)
     train |numIters|, the step size |eta|, return the weight vector (sparse
     feature vector) learned.
 
-    You should implement stochastic gradient descent.  
+    You should implement stochastic gradient descent.
 
     Note: only use the trainExamples for training!
     You should call evaluatePredictor() on both trainExamples and testExamples
@@ -66,19 +68,23 @@ def learnPredictor(trainExamples, testExamples, featureExtractor, numIters, eta)
             featuresExtracted = featureExtractor(a)
             if (dotProduct(weights, featuresExtracted) * b) < 1:
                 increment(weights, b * eta, featuresExtracted)
-    return weights  
+    return weights
 
 data = importData()
-trainExamples = list() 
+trainExamples = list()
 testExamples = list()
-for i in xrange(300): 
+posCount = 0
+for i in xrange(300):
     trainExamples.append(data[i])
+    if data[i][1] == 1:
+        posCount += 1
 # print trainExamples[1:3]
 for i in xrange(300, 310):
     testExamples.append(data[i])
-weights = learnPredictor(trainExamples, testExamples, featureExtractor, 20, 0.01)  
+weights = learnPredictor(trainExamples, testExamples, featureExtractor, 20, 0.01)
 
 trainError = evaluatePredictor(trainExamples, lambda(x) : (1 if dotProduct(featureExtractor(x), weights) >= 0 else -1))
-devError = evaluatePredictor(testExamples, lambda(x) : (1 if dotProduct(featureExtractor(x), weights) >= 0 else -1))    
+devError = evaluatePredictor(testExamples, lambda(x) : (1 if dotProduct(featureExtractor(x), weights) >= 0 else -1))
 print "Official: train error = %s, dev error = %s" % (trainError, devError)
+print "posCount", posCount
 
